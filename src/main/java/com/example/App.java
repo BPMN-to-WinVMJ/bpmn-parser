3345e175d8d11b2428414c2cab6c694bbab582fe
KenKomKom <kenichikomala@gmail.com>
2026-02-27 21:10:16 +0700

    final update

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -23,19 +23,17 @@ public class App {
     public static void main( String[] args ) {
         System.out.println("running");
         try {
-            String bpmnName = "deepbranch";
+            String bpmnName = "studypayment";
             File file = new File(String.format("src\\resource\\%s.bpmn2", bpmnName));
             BPMN bpmn = Parser.parse(file);
             List<Task> tasks = List.copyOf(bpmn.getT().values().stream().toList());
             StartEvent start = bpmn.getEs().values().stream().toList().get(0); // takes start event
             List<Task> startingTask = Util.traverseForward(start.getOut().get(0).getTarget());
             Set<Task> userTasks = new HashSet<>(bpmn.getT().values().stream().filter(x -> x instanceof UserTask).toList());
-            System.out.println("zczc "+ userTasks.stream().map(x->x.getName()).toList());
             for (Task starting : startingTask) {
                 starting.setFromStart(true);
                 userTasks.add(starting);
             }
-            System.out.println("zczc "+ userTasks.stream().map(x->x.getName()).toList());
             
             Parser.loopFold(bpmn);
             
